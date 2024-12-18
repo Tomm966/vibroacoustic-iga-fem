c2f54670d8e2b255920ea2b9231a8644c43db8e9
Tomm966 <landitommaso8@gmail.com>
2024-12-18 17:28:30 +0100

    Update elemsnet2D.m

--- function_TL_CH/elemsnet2D.m
+++ function_TL_CH/elemsnet2D.m
@@ -6,33 +6,24 @@ function [elemsnet] = elemsnet2D(noPtsX,noPtsY)
 % noPtsY: The number of control points in the Y direction.
 % 
 % OUTPUT:
-% net mesh
+% net mesh connectivity
 
-
-    elemsnet = zeros((noPtsX-1)*noPtsY + noPtsX*(noPtsY-1),2);
-    if (noPtsX == 2 && noPtsY > 2)
-        elemsnet([1:noPtsY],1) = [1:2:(2*noPtsY-1)]';
-        elemsnet([1:noPtsY],2) = [2:2:(2*noPtsY)]';
-        elemsnet([1:noPtsY-1]+noPtsY,1) = [1:2:2*noPtsY-3]';
-        elemsnet([1:noPtsY-1]+noPtsY,2) = [3:2:2*noPtsY-1]';
-        elemsnet([1:noPtsY-1]+2*noPtsY-1,1) = [1:2:2*noPtsY-3]'+1;
-        elemsnet([1:noPtsY-1]+2*noPtsY-1,2) = [3:2:2*noPtsY-1]'+1;
-    elseif (noPtsY == 2 && noPtsX > 2)
-        %elemsnet = zeros(noPtsX+noPtsX-1,2);
-        elemsnet([1:noPtsX],1) = [1:noPtsX]';
-        elemsnet([1:noPtsX],2) = [1:noPtsX]'+noPtsX;
-        elemsnet([1:noPtsX-1]+noPtsX,1) = [1:noPtsX-1]';
-        elemsnet([1:noPtsX-1]+noPtsX,2) = [1:noPtsX-1]'+1;
-        elemsnet([1:noPtsX-1]+2*noPtsX-1,1) = [1:noPtsX-1]'+noPtsX;
-        elemsnet([1:noPtsX-1]+2*noPtsX-1,2) = [1:noPtsX-1]'+noPtsX+1;
-    else
-        for i = 1:noPtsY
-            elemsnet( [1:noPtsX-1] + (i-1)*(noPtsY-1),1 ) = [1:noPtsX-1]' + (i-1)*noPtsY;   
-            elemsnet( [1:noPtsX-1] + (i-1)*(noPtsY-1),2 ) = [1:noPtsX-1]' + (i-1)*noPtsY +1  ;
+    elemsnet = [];
+    %the dimension of elemsnet must be elemsnet= zeros((noPtsX-1)*noPtsY+noPtsX*(noPtsY-1),2)
+    % Horizontal connectivity (along x)
+    for row = 0:(noPtsY - 1)
+        for col = 1:(noPtsX - 1)
+            node1 = row * noPtsX + col;
+            node2 = node1 + 1;
+            elemsnet = [elemsnet; node1, node2];
         end
-        for i = 1:noPtsX  
-            elemsnet([1:noPtsY-1] + (i-1)*(noPtsX-1) + (noPtsX-1)*noPtsY,1 ) = [[1:noPtsX:(noPtsX*noPtsY-noPtsX)] + i-1 ]'; 
-            elemsnet([1:noPtsY-1] + (i-1)*(noPtsX-1) + (noPtsX-1)*noPtsY,2 ) = [[1:noPtsX:(noPtsX*noPtsY-noPtsX)] + i-1 + noPtsX ]'; 
+    end
+    % Vertical connectivity (along y)
+    for col = 1:noPtsX
+        for row = 0:(noPtsY - 2)
+            node1 = row * noPtsX + col;
+            node2 = node1 + noPtsX;
+            elemsnet = [elemsnet; node1, node2];
         end
     end
 end
